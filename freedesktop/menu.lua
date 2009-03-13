56b22fae49ecad434002c4e5d0f968aaba0c0d50
Antonio Terceiro <terceiro@softwarelivre.org>
2009-03-13 12:51:42 -0300

    Creating menu inside a function.
    This way users can first require all the needed modules, then set all
    their options and only after that actually create the menu.
    
    Thanks to Sergey (Сергей Миронов) for the idea and initial patch.

--- freedesktop/menu.lua
+++ freedesktop/menu.lua
@@ -7,57 +7,70 @@ local os = os
 
 module("freedesktop.menu", package.seeall)
 
--- the categories and their synonims where shamelessly copied from lxpanel
--- source code.
+function new()
+    -- the categories and their synonyms where shamelessly copied from lxpanel
+    -- source code.
+    local programs = {}
 
-programs = {}
-programs['AudioVideo'] = {}
-programs['Audio'] = {}
-programs['Video'] = {}
-programs['Development'] = {}
-programs['Education'] = {}
-programs['Game'] = {}
-programs['Graphics'] = {}
-programs['Network'] = {}
-programs['Office'] = {}
-programs['Settings'] = {}
-programs['System'] = {}
-programs['Utility'] = {}
-programs['Other'] = {}
+    programs['AudioVideo'] = {}
+    programs['Development'] = {}
+    programs['Education'] = {}
+    programs['Game'] = {}
+    programs['Graphics'] = {}
+    programs['Network'] = {}
+    programs['Office'] = {}
+    programs['Settings'] = {}
+    programs['System'] = {}
+    programs['Utility'] = {}
+    programs['Other'] = {}
 
-for i, program in ipairs(utils.parse_desktop_files({ dir = '/usr/share/applications/' })) do
+    for i, program in ipairs(utils.parse_desktop_files({ dir ='/usr/share/applications/' })) do
 
-    -- check whether to include in the menu
-    if program.show and program.Name and program.cmdline then
-        local target_category = nil
-        if program.categories then
-            for _, category in ipairs(program.categories) do
-                if programs[category] then
-                    target_category = category
-                    break
+        -- check whether to include in the menu
+        if program.show and program.Name and program.cmdline then
+            local target_category = nil
+            if program.categories then
+                for _, category in ipairs(program.categories) do
+                    if programs[category] then
+                        target_category = category
+                        break
+                    end
                 end
             end
-        else
-            target_category = 'Other'
+            if not target_category then
+                target_category = 'Other'
+            end
+            if target_category then
+                table.insert(programs[target_category], { program.Name, program.cmdline, program.icon_path })
+            end
         end
-        if target_category then
-            table.insert(programs[target_category], { program.Name, program.cmdline, program.icon_path })
-        end
+
     end
 
+    local menu = {
+        { "Accessories", programs["Utility"], utils.lookup_icon({ icon = 'applications-accessories.png' }) },
+        { "Development", programs["Development"], utils.lookup_icon({ icon = 'applications-development.png' }) },
+        { "Education", programs["Education"], utils.lookup_icon({ icon = 'applications-science.png' }) },
+        { "Games", programs["Game"], utils.lookup_icon({ icon = 'applications-games.png' }) },
+        { "Graphics", programs["Graphics"], utils.lookup_icon({ icon = 'applications-graphics.png' }) },
+        { "Internet", programs["Network"], utils.lookup_icon({ icon = 'applications-internet.png' }) },
+        { "Multimedia", programs["AudioVideo"], utils.lookup_icon({ icon = 'applications-multimedia.png' }) },
+        { "Office", programs["Office"], utils.lookup_icon({ icon = 'applications-office.png' }) },
+        { "Other", programs["Other"], utils.lookup_icon({ icon = 'applications-other.png' }) },
+        { "Settings", programs["Settings"], utils.lookup_icon({ icon = 'applications-utilities.png' }) },
+        { "System Tools", programs["System"], utils.lookup_icon({ icon = 'applications-system.png' }) },
+    }
+
+    -- Removing empty entries from menu
+    local bad_indexes = {}
+    for index , item in ipairs(menu) do
+        if not item[2] then
+            table.insert(bad_indexes, index)
+        end
+    end
+    for _, index in ipairs(bad_indexes) do
+        table.remove(menu, index)
+    end
+
+    return menu
 end
-
-applications_menu = {
-    { "Accessories", programs["Utility"], utils.lookup_icon({ icon = 'applications-accessories.png' }) },
-    { "Development", programs["Development"], utils.lookup_icon({ icon = 'applications-development.png' }) },
-    { "Education", programs["Education"], utils.lookup_icon({ icon = 'applications-science.png' }) },
-    { "Games", programs["Game"], utils.lookup_icon({ icon = 'applications-games.png' }) },
-    { "Graphics", programs["Graphics"], utils.lookup_icon({ icon = 'applications-graphics.png' }) },
-    { "Internet", programs["Network"], utils.lookup_icon({ icon = 'applications-internet.png' }) },
-    { "Multimedia", programs["AudioVideo"], utils.lookup_icon({ icon = 'applications-multimedia.png' }) },
-    { "Office", programs["Office"], utils.lookup_icon({ icon = 'applications-office.png' }) },
-    { "Other", programs["Other"], utils.lookup_icon({ icon = 'applications-other.png' }) },
-    { "Settings", programs["Settings"], utils.lookup_icon({ icon = 'applications-utilities.png' }) },
-    { "System Tools", programs["System"], utils.lookup_icon({ icon = 'applications-system.png' }) },
-}
-
