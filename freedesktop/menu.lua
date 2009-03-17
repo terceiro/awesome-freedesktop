cded1b73b163dcc6e4d23e98f0e46d1001fd2f0f
Antonio Terceiro <terceiro@softwarelivre.org>
2009-03-17 09:51:54 -0300

    Allow config. of where to look for applications
    Thanks to Alexey Antipovsky for the original patch.

--- freedesktop/menu.lua
+++ freedesktop/menu.lua
@@ -8,10 +8,18 @@ local ipairs = ipairs
 
 module("freedesktop.menu")
 
-function new()
+all_menu_dirs = { '/usr/share/applications/' }
+
+show_generic_name = false
+
+--- Create menus for applications
+-- @param menu_dirs A list of application directories (optional).
+-- @return A prepared menu w/ categories
+function new(arg)
     -- the categories and their synonyms where shamelessly copied from lxpanel
     -- source code.
     local programs = {}
+    local config = arg or {}
 
     programs['AudioVideo'] = {}
     programs['Development'] = {}
@@ -25,27 +33,31 @@ function new()
     programs['Utility'] = {}
     programs['Other'] = {}
 
-    for i, program in ipairs(utils.parse_desktop_files({ dir ='/usr/share/applications/' })) do
-
-        -- check whether to include in the menu
-        if program.show and program.Name and program.cmdline then
-            local target_category = nil
-            if program.categories then
-                for _, category in ipairs(program.categories) do
-                    if programs[category] then
-                        target_category = category
-                        break
+    for i, dir in ipairs(config.menu_dirs or all_menu_dirs) do
+        local entries = utils.parse_desktop_files({dir = dir})
+        for j, program in ipairs(entries) do
+            -- check whether to include in the menu
+            if program.show and program.Name and program.cmdline then
+                if show_generic_name and program.GenericName then
+                    program.Name = program.Name .. ' (' .. program.GenericName .. ')'
+                end
+                local target_category = nil
+                if program.categories then
+                    for _, category in ipairs(program.categories) do
+                        if programs[category] then
+                            target_category = category
+                            break
+                        end
                     end
                 end
-            end
-            if not target_category then
-                target_category = 'Other'
-            end
-            if target_category then
-                table.insert(programs[target_category], { program.Name, program.cmdline, program.icon_path })
+                if not target_category then
+                    target_category = 'Other'
+                end
+                if target_category then
+                    table.insert(programs[target_category], { program.Name, program.cmdline, program.icon_path })
+                end
             end
         end
-
     end
 
     local menu = {
